aaf2bc2106c3428a8d010419eb3dfa9829b4e5a0
Shehan <s3651907@student.rmit.edu.au>
2018-10-11 09:01:27 +1100

    Temp InstructorFeedbackResultsPageUiTest Fix

--- src/test/java/teammates/test/cases/browsertests/InstructorFeedbackResultsPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/InstructorFeedbackResultsPageUiTest.java
@@ -1,17 +1,12 @@
 package teammates.test.cases.browsertests;
 
-
-import teammates.common.util.Const;
 import teammates.test.driver.Priority;
-import teammates.test.pageobjects.InstructorFeedbackResultsPage;
 
 /**
  * SUT: {@link Const.ActionURIs#INSTRUCTOR_FEEDBACK_RESULTS_PAGE}.
  */
 @Priority(-1)
 public class InstructorFeedbackResultsPageUiTest extends BaseUiTestCase {
-
-    private InstructorFeedbackResultsPage resultsPage;
 
     @Override
     protected void prepareTestData() {
