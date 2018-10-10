4908abe69f2e9e98a84f96113ff2be253dea4cba
Shehan <s3651907@student.rmit.edu.au>
2018-10-11 08:49:43 +1100

    Temp InstructorFeedbackResultsPageUiTest Fix

--- src/test/java/teammates/test/cases/browsertests/InstructorFeedbackResultsPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/InstructorFeedbackResultsPageUiTest.java
@@ -1,21 +1,8 @@
 package teammates.test.cases.browsertests;
 
-import java.io.File;
 
-import org.openqa.selenium.By;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
-import teammates.common.util.AppUrl;
 import teammates.common.util.Const;
-import teammates.common.util.JsonUtils;
-import teammates.common.util.SanitizationHelper;
-import teammates.test.driver.BackDoor;
-import teammates.test.driver.FileHelper;
 import teammates.test.driver.Priority;
-import teammates.test.driver.TestProperties;
-import teammates.test.pageobjects.InstructorFeedbackEditPage;
 import teammates.test.pageobjects.InstructorFeedbackResultsPage;
 
 /**
